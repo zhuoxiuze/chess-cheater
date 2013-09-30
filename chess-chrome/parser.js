54c41ea68df40d0037a6ddef27f58c7cafc97522
Daylen Yang <services@daylenyang.com>
2013-09-29 17:23:42 -0700

    Improved stability, looks better
    Control panel has styling
    Perfect opponent move detection
    Control panel has status text

--- chess-chrome/parser.js
+++ chess-chrome/parser.js
@@ -1,24 +1,15 @@
 var controlpanel = '<div id="CHESS_SETTINGS">\
-<a href="#" id="CHESS_SETTINGS_START">Start</a><br>\
-<a href="#" id="CHESS_SETTINGS_STOP">Stop</a><br>\
-<a href="#" id="CHESS_SETTINGS_RESET">Reset</a>\
+<h1>Chess Cheater Deluxe Pro Alpha</h1>\
+<a href="#" id="CHESS_SETTINGS_START">Auto-Move</a>\
+<a href="#" id="CHESS_SETTINGS_RESET">Clear Move History</a>\
+<p>Status Text</p>\
 </div>';
 
 $('body').append(controlpanel);
-$('#CHESS_SETTINGS').css({
-	position: 'absolute',
-	bottom: 0,
-	right: 0,
-	height: '200px',
-	width: '300px',
-	background: '#be0c0b',
-	'z-index': '100'
-});
-$('#CHESS_SETTINGS a').css({
-	color: 'white',
-	'font-family': 'Segoe UI',
-	'font-size': '20px'
-});
+
+function status(text) {
+	$('#CHESS_SETTINGS p').text(text);
+}
 
 function get_letter(pos, bside) {
 	var num = get_number(pos, bside) - 1;
@@ -36,82 +27,72 @@ function get_number_reverse(pos, bside) {
 }
 
 $("#CHESS_SETTINGS_START").click(function() {
-	//alert('prepare for awesomeness');
-	//$('body').bind('DOMSubtreeModified', function() {
-		// find out whether i am top or bottom
-		var mySide = $(".plyrb:contains('electrocuter')").attr('id');
-		var currSide = $('.timerin.active').parent().attr('id');
-		//var moved = false;
-		//console.log(mySide);
-		//console.log(currSide);
-		//if (mySide != currSide) {
-		//	moved = false;
-		//}
-		if (true) {
-			// set position ... done
-			var btop = $('#chessboard_dummy').offset().top;
-			var bleft = $('#chessboard_dummy').offset().left;
-			var bside = $('#chessboard_dummy').width();
-			$.get('http://localhost:5000/position', {boardTop: btop + 60,
-				boardLeft: bleft,
-				boardSide: bside});
+	var username = $('#layout_top_username .chess_com_username_link').text();
 
-			// add move ... todo
-			// yellow squares
-			var squares = $('div').filter(function() {
-				return $(this).css('border') == '3px solid rgb(255, 255, 51)';
+	// Am I top or bottom?
+	var mySide = $(".plyrb:contains('"+ username + "')").attr('id');
+	var currSide = $('.timerin.active').parent().attr('id');
+
+	if (mySide == currSide) {
+		// Board positioning
+		var btop = $('#chessboard_dummy').offset().top;
+		var bleft = $('#chessboard_dummy').offset().left;
+		var bside = $('#chessboard_dummy').width();
+		$.get('http://localhost:5000/position', {boardTop: btop + 60,
+			boardLeft: bleft,
+			boardSide: bside});
+
+		// The yellow squares
+		var squares = $('div').filter(function() {
+			return $(this).css('border') == '3px solid rgb(255, 255, 51)';
+		});
+		if (squares.length != 2) {
+			$.get('http://localhost:5000/makemove?', function() {
+				status('Made the first move');
 			});
-			if (squares.length != 2) {
-				$.get('http://localhost:5000/makemove?');
-				//moved = true;
-				return;
-			}
-			var num1 = get_number_reverse(parseInt($(squares[0]).css('top')), bside);
-			var let1 = get_letter(parseInt($(squares[0]).css('left')), bside);
-			var num2 = get_number_reverse(parseInt($(squares[1]).css('top')), bside);
-			var let2 = get_letter(parseInt($(squares[1]).css('left')), bside);
-			var sq1 = let1 + num1;
-			var sq2 = let2 + num2;
+			return;
+		}
+		var num1 = get_number_reverse(parseInt($(squares[0]).css('top')), bside);
+		var let1 = get_letter(parseInt($(squares[0]).css('left')), bside);
+		var num2 = get_number_reverse(parseInt($(squares[1]).css('top')), bside);
+		var let2 = get_letter(parseInt($(squares[1]).css('left')), bside);
+		var sq1 = let1 + num1;
+		var sq2 = let2 + num2;
 
-			// convert top and left to notation
-			// var bcell = bside / 8;
-
-
-			// last notation
-
-			var finalPos = $('.notationVertical:last-of-type .mhl a').text().replace('+','');
-			if (finalPos == "O-O") {
-				// todo castle
-			} else if (finalPos == "O-O-O") {
-
-			}
-			finalPos=finalPos.substring(finalPos.length - 2);
-			// queening todo
-
-			var combined;
-			if (sq1 == finalPos) {
-				combined = sq2 + finalPos;
+		var finalPos = $('.notationVertical:last-of-type .mhl a').text().replace('+','');
+		if (finalPos.indexOf("=") != -1) {
+			finalPos = finalPos.substring(0,2) + finalPos.substring(3,4).toLowerCase();
+		} else if (finalPos == "O-O" || finalPos == "O-O-O") {
+			if (sq1.indexOf("e") != -1) {
+				finalPos = sq2
 			} else {
-				combined = sq1 + finalPos;
+				finalPos = sq1
 			}
-			$.get('http://localhost:5000/addmove', {move: combined}, function() {
-				alert('added move ' + combined);
-				// when adding move returns
-				$.get('http://localhost:5000/makemove?');
-			});
-
-			
+		} else {
+			finalPos=finalPos.substring(finalPos.length - 2);
 		}
 
-		
-	//});
-});
-
-$("#CHESS_SETTINGS_STOP").click(function() {
-	$('body').unbind('DOMSubtreeModified');
+		var combined;
+		if (sq1 == finalPos) {
+			combined = sq2 + finalPos;
+		} else {
+			combined = sq1 + finalPos;
+		}
+		status('Enemy did ' + combined);
+		$.get('http://localhost:5000/addmove', {move: combined}, function() {
+			$.get('http://localhost:5000/makemove?', function(data) {
+				status('Stockfish did ' + data);
+			});
+		});
+	} else {
+		status("You are " + mySide + " but it is " + currSide + "'s turn");
+	}
+			
 });
 
 $("#CHESS_SETTINGS_RESET").click(function() {
-	$.get('http://localhost:5000/clear?');
-	$('#cheat-stat').html('');
+	$.get('http://localhost:5000/clear?', function() {
+		status('Cleared');
+	});
+	
 });
