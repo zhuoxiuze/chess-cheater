3798e1a033990ca096563712b0684176136e9e82
Daylen Yang <services@daylenyang.com>
2013-12-04 20:10:08 -0800

    Mouse moving now works

--- chess-api/flask/app.py
+++ chess-api/flask/app.py
@@ -5,8 +5,29 @@ import engine
 import ctypes
 import time
 
-def click2(x, y):
-	pass
+from Quartz.CoreGraphics import CGEventCreateMouseEvent
+from Quartz.CoreGraphics import CGEventPost
+from Quartz.CoreGraphics import kCGEventMouseMoved
+from Quartz.CoreGraphics import kCGEventLeftMouseDown
+from Quartz.CoreGraphics import kCGEventLeftMouseDown
+from Quartz.CoreGraphics import kCGEventLeftMouseUp
+from Quartz.CoreGraphics import kCGMouseButtonLeft
+from Quartz.CoreGraphics import kCGHIDEventTap
+
+def mouseEvent(type, posx, posy):
+        theEvent = CGEventCreateMouseEvent(
+                    None, 
+                    type, 
+                    (posx,posy), 
+                    kCGMouseButtonLeft)
+        CGEventPost(kCGHIDEventTap, theEvent)
+
+def click2(posx, posy):
+	# uncomment this line if you want to force the mouse 
+    # to MOVE to the click location first (I found it was not necessary).
+    #mouseEvent(kCGEventMouseMoved, posx,posy);
+    mouseEvent(kCGEventLeftMouseDown, posx,posy);
+    mouseEvent(kCGEventLeftMouseUp, posx,posy);
 app = Flask(__name__)
 
 # board positions
